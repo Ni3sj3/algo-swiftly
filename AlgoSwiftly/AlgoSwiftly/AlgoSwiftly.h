2588ea31458b8db02d66865a84e6ce42c02c3d6c
Niesje Dowell <niesjedowell@Niesjevans-MBP.fios-router.home>
2018-07-23 08:27:38 -0400

    create project

---
+++ AlgoSwiftly/AlgoSwiftly/AlgoSwiftly.h
@@ -0,0 +1,19 @@
+//
+//  AlgoSwiftly.h
+//  AlgoSwiftly
+//
+//  Created by Niesje Dowell on 7/23/18.
+//  Copyright © 2018 Niesje Dowell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlgoSwiftly.
+FOUNDATION_EXPORT double AlgoSwiftlyVersionNumber;
+
+//! Project version string for AlgoSwiftly.
+FOUNDATION_EXPORT const unsigned char AlgoSwiftlyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlgoSwiftly/PublicHeader.h>
+
+
